db319ff0de67b3f5997e158d7fe3f2635e51d6c2
Railyard Developer <dossier.railyard@gmail.com>
2019-09-02 16:55:12 -0700

    everything from dev

--- wp-content/themes/oceanwp/woocommerce/single-product/product-image.php
+++ wp-content/themes/oceanwp/woocommerce/single-product/product-image.php
@@ -34,7 +34,6 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 ) );
 ?>
 <div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
-  <h1>Jonathan Can cut up code!!!!</h1>
 	<figure class="woocommerce-product-gallery__wrapper">
 		<?php
 		if ( $product->get_image_id() ) {
